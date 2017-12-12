6848eecfe43070d482e04083c07ab2b4e4d4a932
Ha-Doan <doanthanhha@gmail.com>
2017-12-12 21:56:39 +0100

    Set up sign in, sign up, sign out for React app.

---
+++ src/containers/ClassesContainer.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react'
+
+class ClassesContainer extends PureComponent{
+  render() {
+    return (
+      <div className="ClassesContainer">
+      </div>
+    )
+  }
+
+}
+export default ClassesContainer
---
+++ src/containers/index.js
@@ -0,0 +1,10 @@
+
+import ClassesContainer from './ClassesContainer'
+import SignIn from './SignIn'
+import SignUp from './SignUp'
+
+export {
+  ClassesContainer,
+  SignIn,
+  SignUp,
+}
