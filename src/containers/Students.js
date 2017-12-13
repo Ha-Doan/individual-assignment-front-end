7f41f8d6d3b723dc2aedc04acc386deca0dcb5bb
Ha-Doan <doanthanhha@gmail.com>
2017-12-13 18:20:53 +0100

    Add propTypes for Students.

--- src/containers/Students.js
+++ src/containers/Students.js
@@ -2,7 +2,20 @@ import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
+const evaluationShape = PropTypes.shape({
+  date:  PropTypes.string,
+  color:  PropTypes.string,
+})
+
+const studentShape = PropTypes.shape({
+  _id: PropTypes.string.isRequired,
+  evaluations: PropTypes.arrayOf(evaluationShape),
+})
+
 class Students extends PureComponent{
+  static propTypes = {
+    students: PropTypes.arrayOf(studentShape),
+  }
   render() {
     return()
   }
